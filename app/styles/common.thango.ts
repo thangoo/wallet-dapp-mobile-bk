e571248cbb1c19b46c897a5684850a94d348ecfa
nnhi3113 <116545890+nnhi3113@users.noreply.github.com>
2023-03-17 15:18:06 +0700

    add color tvn.primary.default and text.mute

--- app/styles/common.thango.ts
+++ app/styles/common.thango.ts
@@ -1,18 +1,22 @@
 export const thangoLight = {
   ['tvn.black']: '#222222',
-  ['tvn.blue']: '#277EFF',  // colors.primary.default
+  ['tvn.blue']: '#277EFF', // colors.primary.default
   ['tvn.white']: '#FFFFFF',
   ['tvn.grayLight']: '#999999',
   ['tvn.grayDark']: ' #333333',
   ['tvn.background.default']: '#222222', // colors.background.default
+  ['tvn.primary.default']: '#277EFF',
   ['tvn.innerBackground.default']: '#222222', // colors.innerBackground.default
+  ['tvn.text.muted']: '#FFFFFF',
 };
 export const thangoDark = {
   ['tvn.black']: '#222222',
-  ['tvn.blue']: '#277EFF',  // colors.primary.default
+  ['tvn.blue']: '#277EFF', // colors.primary.default
   ['tvn.white']: '#FFFFFF',
   ['tvn.grayLight']: '#999999',
   ['tvn.grayDark']: ' #333333',
   ['tvn.background.default']: '#222222', // colors.background.default
+  ['tvn.primary.default']: '#277EFF',
   ['tvn.innerBackground.default']: '#222222', // colors.innerBackground.default
+  ['tvn.text.muted']: '#FFFFFF',
 };
