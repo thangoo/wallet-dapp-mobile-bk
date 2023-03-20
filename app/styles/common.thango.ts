b0e59aa1ddc5660b373af6d76300c07f46b30034
Thuan Hoang <thuan@thangovn.com>
2023-03-20 11:45:59 +0700

    add color name from figma

--- app/styles/common.thango.ts
+++ app/styles/common.thango.ts
@@ -9,6 +9,19 @@ export const thangoLight = {
   ['tvn.innerBackground.default']: '#222222', // colors.innerBackground.default
   ['tvn.text.muted']: '#FFFFFF',
   ['tvn.text.default']: '#FFFFFF',
+
+  // COLOR NAME FIGMA
+  ['tvn.gray.01']: '#FFFFFF',
+  ['tvn.gray.02']: '#F8F8F8',
+  ['tvn.gray.04']: '#DCDCDC',
+  ['tvn.gray.05']: '#CECECE',
+  ['tvn.gray.06']: '#BABABA',
+  ['tvn.gray.10']: '#333333',
+  ['tvn.light_gray_blue']: '#F3F6FB',
+  ['tvn.dark_gray_blue']: '#97A8C1',
+  ['tvn.primary.blue']: '#277EFF',
+  ['tvn.status.orange']: '#FF8717',
+  ['tvn.status.red']: '#FF2517',
 };
 export const thangoDark = {
   ['tvn.black']: '#222222',
@@ -21,4 +34,18 @@ export const thangoDark = {
   ['tvn.innerBackground.default']: '#222222', // colors.innerBackground.default
   ['tvn.text.muted']: '#FFFFFF',
   ['tvn.text.default']: '#FFFFFF',
+
+  // COLOR NAME FIGMA
+  // TODO: waiting design color dark mode
+  ['tvn.gray.01']: '#FFFFFF',
+  ['tvn.gray.02']: '#F8F8F8',
+  ['tvn.gray.04']: '#DCDCDC',
+  ['tvn.gray.05']: '#CECECE',
+  ['tvn.gray.06']: '#BABABA',
+  ['tvn.gray.10']: '#333333',
+  ['tvn.light_gray_blue']: '#F3F6FB',
+  ['tvn.dark_gray_blue']: '#97A8C1',
+  ['tvn.primary.blue']: '#277EFF',
+  ['tvn.status.orange']: '#FF8717',
+  ['tvn.status.red']: '#FF2517',
 };
