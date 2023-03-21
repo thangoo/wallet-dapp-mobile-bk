447a44f3aea3176cb09ed78bc6b1be39675cc99f
Thuan Hoang <thuan@thangovn.com>
2023-03-21 10:22:21 +0700

    update: new ui signin

--- app/components/Views/Login/PassCode.tsx
+++ app/components/Views/Login/PassCode.tsx
@@ -1,86 +1,113 @@
+import React, { useImperativeHandle, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
 
 import {
   CodeField,
   Cursor,
+  MaskSymbol,
   useBlurOnFulfill,
   useClearByFocusCell,
+  isLastFilledCell,
 } from 'react-native-confirmation-code-field';
+import { useAppTheme } from '../../../../app/util/theme';
 
+export const refPassCode = React.createRef();
 const CELL_COUNT = 6;
-const PassCode = () => {
+const PassCode = ({
+  onSubmitEditing,
+}: {
+  onSubmitEditing: (passcode: string) => void;
+}) => {
   const [value, setValue] = useState('');
   const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
   const [props, getCellOnLayoutHandler] = useClearByFocusCell({
     value,
     setValue,
   });
+  const { colors } = useAppTheme();
+  const styles = createStyles(colors);
+
+  useImperativeHandle(refPassCode, () => ({
+    clearValue: () => setValue(''),
+  }));
+
+  const onBlur = () => {
+    if (value.length === CELL_COUNT) {
+      onSubmitEditing(value);
+    }
+  };
+
+  const renderCell = ({
+    index,
+    symbol,
+    isFocused,
+  }: {
+    index: number;
+    symbol: string;
+    isFocused: boolean;
+  }) => {
+    let textChild = null;
+
+    if (symbol) {
+      textChild = (
+        <MaskSymbol
+          maskSymbol="●"
+          isLastFilledCell={isLastFilledCell({ index, value })}
+        >
+          {symbol}
+        </MaskSymbol>
+      );
+    } else if (isFocused) {
+      textChild = <Cursor />;
+    }
+
+    return (
+      <View style={styles.wrapText} key={index}>
+        <Text
+          style={[styles.cell, isFocused && styles.focusCell, {}]}
+          onLayout={getCellOnLayoutHandler(index)}
+        >
+          {textChild}
+        </Text>
+      </View>
+    );
+  };
 
   return (
-    <>
-      <CodeField
-        ref={ref}
-        {...props}
-        // Use `caretHidden={false}` when users can't paste a text value, because context menu doesn't appear
-        value={value}
-        onChangeText={setValue}
-        cellCount={CELL_COUNT}
-        rootStyle={styles.codeFieldRoot}
-        keyboardType="number-pad"
-        textContentType="oneTimeCode"
-        renderCell={({ index, symbol, isFocused }) => (
-          <View
-            style={{
-              borderWidth: 1,
-              borderStyle: 'dashed',
-              marginRight: 8,
-              borderRadius: 8,
-              width: 48,
-            }}
-          >
-            <Text
-              key={index}
-              style={[styles.cell, isFocused && styles.focusCell, {}]}
-              onLayout={getCellOnLayoutHandler(index)}
-            >
-              {symbol || (isFocused ? <Cursor /> : null)}
-            </Text>
-          </View>
-        )}
-        textInputStyle={{
-          height: 64,
-          padding: 20,
-        }}
-      />
-    </>
+    <CodeField
+      ref={ref}
+      {...props}
+      // Use `caretHidden={false}` when users can't paste a text value, because context menu doesn't appear
+      value={value}
+      onChangeText={setValue}
+      cellCount={CELL_COUNT}
+      keyboardType="number-pad"
+      textContentType="oneTimeCode"
+      secureTextEntry
+      onBlur={onBlur}
+      renderCell={renderCell}
+    />
   );
 };
 
 export default PassCode;
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    padding: 20,
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 30,
-  },
-  codeFieldRoot: {
-    // marginTop: 20,
-    height: 64,
-    // borderWidth: 1,
-  },
-  cell: {
-    width: 48,
-    height: 64,
-    fontSize: 24,
-    textAlign: 'center',
-    lineHeight: 64,
-  },
-  focusCell: {
-    // borderColor: '#000',
-  },
-});
+const createStyles = (colors: any) =>
+  StyleSheet.create({
+    cell: {
+      fontSize: 24,
+      textAlign: 'center',
+      lineHeight: 64,
+      height: 64,
+    },
+    focusCell: {},
+    wrapText: {
+      borderWidth: 1,
+      borderStyle: 'dashed',
+      borderColor: colors['tvn.gray.06'],
+      borderRadius: 8,
+      height: 64,
+      flex: 1,
+      marginHorizontal: 4,
+    },
+  });
