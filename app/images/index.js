923adc2add7291361d52a751def8c5e6cd4848c4
Thuan Hoang <thuan@thangovn.com>
2023-03-22 09:35:08 +0700

    add new icon

--- app/images/index.js
+++ app/images/index.js
@@ -1,4 +1,15 @@
 const logo = require('./logo.png');
 const vector = require('./vector.png');
+const shield_warning_icon = require('./shield_warning_icon.png');
+const copy_icon = require('./copy_icon.png');
+const shield_warning_2_icon = require('./shield_warning_2_icon.png');
+const arrow_right_icon = require('./arrow_right_icon.png');
 
-export { logo, vector };
+export {
+  arrow_right_icon,
+  shield_warning_2_icon,
+  logo,
+  vector,
+  shield_warning_icon,
+  copy_icon,
+};
