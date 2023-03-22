c2701319378ab912400a4e6908e981298bbb161e
Thuan Hoang <thuan@thangovn.com>
2023-03-22 21:01:54 +0700

    add icon

--- app/images/index.js
+++ app/images/index.js
@@ -8,10 +8,13 @@ const logoSplashScreen = require('./logoSplash.png');
 const vectorSplashScreen = require('./vectorSplash.png');
 const backgroundOnboarding = require('./background1-onboarding.png');
 const imgWalletOnboarding = require('./img-wallet-onboarding.png');
-
 const imgSecretPhrase = require('./img-secret-phrase-screen.png');
+const check_box_empty = require('./check_box_empty.png');
+const check_box_blue = require('./check_box_blue.png');
 
 export {
+  check_box_blue,
+  check_box_empty,
   arrow_right_icon,
   shield_warning_2_icon,
   logo,
