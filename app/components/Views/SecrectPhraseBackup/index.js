7fad4972016aa63efe0106575792b96f3e79725f
nnhi3113 <116545890+nnhi3113@users.noreply.github.com>
2023-03-22 17:01:19 +0700

    screen phrase

---
+++ app/components/Views/SecrectPhraseBackup/index.js
@@ -0,0 +1,230 @@
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Platform,
+} from 'react-native';
+import { MetaMetricsEvents } from '../../../core/Analytics';
+import StyledButton from '../../UI/StyledButton';
+import { fontStyles, baseStyles } from '../../../styles/common';
+import { strings } from '../../../../locales/i18n';
+import FadeOutOverlay from '../../UI/FadeOutOverlay';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import { getTransparentOnboardingNavbarOptions } from '../../UI/Navbar';
+import { PREVIOUS_SCREEN, ONBOARDING } from '../../../constants/navigation';
+import Device from '../../../util/device';
+import { saveOnboardingEvent } from '../../../actions/onboarding';
+import { connect } from 'react-redux';
+import { trackEvent } from '../../../util/analyticsV2';
+import { ThemeContext, mockTheme } from '../../../util/theme';
+import {
+  WELCOME_SCREEN_CAROUSEL_TITLE_ID,
+  WELCOME_SCREEN_GET_STARTED_BUTTON_ID,
+  WELCOME_SCREEN_CAROUSEL_CONTAINER_ID,
+  WALLET_SETUP_CREATE_NEW_WALLET_BUTTON_ID,
+} from '../../../../wdio/screen-objects/testIDs/Screens/WelcomeScreen.testIds';
+import generateTestId from '../../../../wdio/utils/generateTestId';
+
+import {
+  backgroundOnboarding,
+  imgSecretPhrase,
+  imgWalletOnboarding,
+} from 'images/index';
+
+const IMAGE_3_RATIO = 215 / 315;
+const IMAGE_2_RATIO = 222 / 239;
+const IMAGE_1_RATIO = 285 / 203;
+const DEVICE_WIDTH = Dimensions.get('window').width;
+
+const IMG_PADDING = Device.isIphoneX() ? 100 : Device.isIphone5S() ? 180 : 220;
+
+const createStyles = (colors) =>
+  StyleSheet.create({
+    scroll: {
+      flexGrow: 1,
+    },
+    wrapper: {
+      flex: 1,
+      position: 'relative',
+    },
+    title: {
+      fontSize: 28,
+      marginBottom: 16,
+      color: colors['tvn.gray.10'],
+      justifyContent: 'flex-end',
+      textAlign: 'center',
+      ...fontStyles.bold,
+    },
+    subtitle: {
+      fontSize: 16,
+      lineHeight: 19,
+      color: colors['tvn.gray.10'],
+      justifyContent: 'center',
+      textAlign: 'center',
+      ...fontStyles.normal,
+    },
+    ctas: {
+      paddingHorizontal: 40,
+      paddingBottom: Device.isIphoneX() ? 40 : 20,
+    },
+    ctaWrapper: {},
+
+    // eslint-disable-next-line react-native/no-unused-styles
+    carouselImage: {
+      width: '97%',
+      height: '75%',
+    },
+    // eslint-disable-next-line react-native/no-unused-styles
+    carouselImage2: {
+      width: DEVICE_WIDTH - IMG_PADDING,
+      height: (DEVICE_WIDTH - IMG_PADDING) * IMAGE_2_RATIO,
+    },
+    // eslint-disable-next-line react-native/no-unused-styles
+    carouselImage3: {
+      width: DEVICE_WIDTH - 60,
+      height: (DEVICE_WIDTH - 60) * IMAGE_3_RATIO,
+    },
+    carouselImageWrapper: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+    },
+    circle: {
+      width: 6,
+      height: 6,
+      borderRadius: 8 / 2,
+      backgroundColor: colors['tvn.gray.04'],
+      marginHorizontal: 4,
+    },
+    solidCircle: {
+      width: 24,
+      backgroundColor: colors['tvn.primary.blue'],
+    },
+    progessContainer: {
+      flexDirection: 'row',
+      alignSelf: 'center',
+      marginBottom: 30,
+    },
+    tab: {
+      marginHorizontal: 32,
+    },
+  });
+
+const carousel_images = [
+  imgWalletOnboarding,
+  imgWalletOnboarding,
+  imgWalletOnboarding,
+  imgWalletOnboarding,
+];
+
+/**
+ * View that is displayed to first time (new) users
+ */
+const SecretPhrase = (props) => {
+  const [currentTab, setCurrentTab] = useState(1);
+
+  const onPressCreate = () => {
+    props.navigation.navigate('Legal', {
+      [PREVIOUS_SCREEN]: ONBOARDING,
+    });
+  };
+
+  const renderTabBar = () => <View />;
+
+  const { colors } = useContext(ThemeContext) || mockTheme.colors;
+
+  const updateNavBar = () => {
+    props.navigation.setOptions(getTransparentOnboardingNavbarOptions(colors));
+  };
+
+  useEffect(() => {
+    updateNavBar();
+    trackEvent(MetaMetricsEvents.ONBOARDING_WELCOME_MESSAGE_VIEWED);
+  }, []);
+
+  useEffect(() => {
+    updateNavBar();
+  });
+
+  const styles = createStyles(colors);
+
+  return (
+    <View style={baseStyles.flexGrow} testID={'secret-phrase-screen--screen'}>
+      <View
+        style={{
+          position: 'absolute',
+          top: -30,
+        }}
+      >
+        <Image
+          source={backgroundOnboarding}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <View
+          style={styles.wrapper}
+          {...generateTestId(Platform, WELCOME_SCREEN_CAROUSEL_CONTAINER_ID)}
+        >
+          <ScrollableTabView
+            style={styles.scrollTabs}
+            renderTabBar={this.renderTabBar}
+            onChangeTab={this.onChangeTab}
+          >
+            <View style={[baseStyles.flexGrow]}>
+              <View style={styles.carouselImageWrapper}>
+                <Image
+                  source={imgSecretPhrase}
+                  style={styles.carouselImage}
+                  resizeMode={'contain'}
+                  testID={`carousel-image`}
+                />
+                <View style={styles.tab}>
+                  <Text style={styles.title}>
+                    {strings(`onboarding_carousel.title1`)}
+                  </Text>
+                  <Text style={styles.subtitle}>
+                    {strings(`onboarding_carousel.subtitle1`)}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </ScrollableTabView>
+        </View>
+      </ScrollView>
+      <View style={styles.ctas}>
+        <View style={styles.ctaWrapper}>
+          <StyledButton
+            type={'blue'}
+            onPress={this.onPressCreate}
+            testID={WALLET_SETUP_CREATE_NEW_WALLET_BUTTON_ID}
+          >
+            {strings('onboarding.start_exploring_now')}
+          </StyledButton>
+          <StyledButton
+            type={'transparent-blue'}
+            onPress={this.onPressGetStarted}
+            testID={WELCOME_SCREEN_GET_STARTED_BUTTON_ID}
+          >
+            {strings('onboarding.already_have_wallet')}
+          </StyledButton>
+        </View>
+      </View>
+      <FadeOutOverlay />
+    </View>
+  );
+};
+
+SecretPhrase.contextType = ThemeContext;
+
+const mapDispatchToProps = (dispatch) => ({
+  saveOnboardingEvent: (...eventArgs) =>
+    dispatch(saveOnboardingEvent(eventArgs)),
+});
+
+export default connect(null, mapDispatchToProps)(SecretPhrase);
