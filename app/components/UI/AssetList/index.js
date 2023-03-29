20cc83047efb4b65f381936131510a062f72bb0e
Ly Minh <minduc2020@gmail.com>
2023-03-30 01:40:55 +0700

    Add switch to toggle token select

--- app/components/UI/AssetList/index.js
+++ app/components/UI/AssetList/index.js
@@ -1,12 +1,14 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 import { View, StyleSheet, Platform } from 'react-native';
 import PropTypes from 'prop-types';
+import SwitchToggle from "react-native-switch-toggle";
 import { strings } from '../../../../locales/i18n';
 import StyledButton from '../StyledButton'; // eslint-disable-line  import/no-unresolved
 import AssetIcon from '../AssetIcon';
 import { colors as importedColors, fontStyles } from '../../../styles/common';
 import Text from '../../Base/Text';
 import generateTestId from '../../../../wdio/utils/generateTestId';
+import { useTheme } from '../../../util/theme';
 import { TOKEN_RESULTS_LIST_ID } from '../../../../wdio/screen-objects/testIDs/Screens/AssetSearch.testIds';
 
 const styles = StyleSheet.create({
@@ -50,6 +52,19 @@ const styles = StyleSheet.create({
   normalText: {
     ...fontStyles.normal,
   },
+  switch: {
+    containerStyle: {
+      width: 44,
+      height: 24,
+      borderRadius: 25,
+      padding: 5,
+    },
+    circleStyle: {
+      width: 19,
+      height: 19,
+      borderRadius: 20,
+    }
+  }
 });
 
 /**
@@ -75,13 +90,23 @@ export default class AssetList extends PureComponent {
     searchQuery: PropTypes.string,
   };
 
+
+
   onToggleAsset = (key) => {
     const { searchResults, handleSelectAsset } = this.props;
     handleSelectAsset(searchResults[key]);
   };
 
+
+
+
   render = () => {
     const { searchResults = [], handleSelectAsset, selectedAsset } = this.props;
+
+    toggleToken = (key) => {
+      // console.log('### searchResults[key]: ', searchResults[key]);
+      handleSelectAsset(searchResults[key]);
+    }
 
     return (
       <View style={styles.rowWrapper} testID={'add-searched-token-screen'}>
@@ -96,32 +121,54 @@ export default class AssetList extends PureComponent {
           </Text>
         ) : null}
         {searchResults.slice(0, 6).map((_, i) => {
-          const { symbol, name, address, iconUrl } = searchResults[i] || {};
-          const isSelected = selectedAsset && selectedAsset.address === address;
-
           return (
             <StyledButton
               type={'tokenList'}
               containerStyle={styles.item}
-              onPress={() => handleSelectAsset(searchResults[i])} // eslint-disable-line
+              // onPress={() => handleSelectAsset(searchResults[i])} // eslint-disable-line
               key={i}
               {...generateTestId(Platform, TOKEN_RESULTS_LIST_ID)}
             >
-              <View style={styles.assetListElement}>
-                <AssetIcon
-                  address={address}
-                  logo={iconUrl}
-                  customStyle={styles.tokenIcon}
-                />
-                <View style={styles.textWrapper}>
-                  <Text style={styles.text}>{name}</Text>
-                  <Text style={styles.symbol}>{symbol}</Text>
-                </View>
-              </View>
+              <TokenItem item={searchResults[i]} key={i} toggleToken={toggleToken} selectedAsset={selectedAsset} />
             </StyledButton>
           );
         })}
       </View>
     );
   };
+}
+
+const TokenItem = ({ item, key, toggleToken, selectedAsset }) => {
+  const { colors } = useTheme();
+  const [isSelected, setIsSelected] = useState(selectedAsset && selectedAsset.address === address);
+  const { symbol, name, address, iconUrl } = item || {};
+
+  const toggleSwitch = (key) => {
+    // isSelected = !isSelected;
+    setIsSelected(!isSelected);
+    toggleToken(key);
+  }
+
+  return (
+    <View style={styles.assetListElement}>
+      <AssetIcon
+        address={address}
+        logo={iconUrl}
+        customStyle={styles.tokenIcon}
+      />
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.symbol}>{symbol}</Text>
+      </View>
+      <SwitchToggle
+        switchOn={isSelected}
+        onPress={() => toggleSwitch(key)}
+        circleColorOff={colors['tvn.gray.06']}
+        circleColorOn={colors['tvn.primary.blue']}
+        backgroundColorOn={colors['tvn.white']}
+        backgroundColorOff={colors['tvn.white']}
+        containerStyle={styles.switch.containerStyle}
+        circleStyle={styles.switch.circleStyle}
+      />
+    </View>)
 }
